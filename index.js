1485c42b18a3df8dc4786aef6fc63ef68d62f4c1
sachin <sachinprajapati0203@gmail.com>
2024-11-06 19:01:49 +0530

    Add the button which take to the Top

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
+
+// Add the Dark Mode
+const body = document.body;
+const themeBtn = document.querySelector(".theme-btn");
+const sunIcon = document.querySelector(".sun");
+const moonIcon = document.querySelector(".moon");
+
+// apply saved theme on page reload
+const savedTheme = localStorage.getItem("theme") || "light";
+body.classList.add(`${savedTheme}-theme`);
+sunIcon.classList.toggle("mode", savedTheme === "light");
+moonIcon.classList.toggle("mode", savedTheme === "dark");
+
+// Toggle theme and save 
+themeBtn.addEventListener("click", () => {
+  const isLightTheme = body.classList.contains("light-theme");
+  body.classList.toggle("light-theme", !isLightTheme);
+  body.classList.toggle("dark-theme", isLightTheme);
+  sunIcon.classList.toggle("mode", !isLightTheme);
+  moonIcon.classList.toggle("mode", isLightTheme);
+  localStorage.setItem("theme", isLightTheme ? "dark" : "light");
+});
+
 // add the drop-down button
 let btn = document.querySelector('.toggle-btn');
 let btnIcon = document.querySelector('.toggle-btn i');
 let dropDownBar = document.querySelector('.dropdown-nav');
-btn.addEventListener('click', () => {
-  dropDownBar.classList.toggle('open');
-  let isOpen = dropDownBar.classList.contains('open');
-  btnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
-})
 
+// It display the button when scrolling down
 
+window.onscroll = function(){
+  const backToTop = document.querySelector("#backToTop");
+  if(document.body.scrollTop>100 || document.documentElement.scrollTop >100){
+    backToTop.style.display= "block";
+  }
+  else{
+    backToTop.style.display= "none";
+  }
+}
+
+function scrollToTop(){
+  window.scrollTo({
+    top:0,
+    behavior : "smooth",
+  });
+};
 
 const boxes = document.querySelectorAll(".container-items");
 
@@ -42,25 +77,3 @@ function toggleTitle(index){
     }
   });
 }
-
-
-const body = document.body;
-const themeBtn = document.querySelector(".theme-btn");
-const sunIcon = document.querySelector(".sun");
-const moonIcon = document.querySelector(".moon");
-
-// apply saved theme on page reload
-const savedTheme = localStorage.getItem("theme") || "light";
-body.classList.add(`${savedTheme}-theme`);
-sunIcon.classList.toggle("mode", savedTheme === "light");
-moonIcon.classList.toggle("mode", savedTheme === "dark");
-
-// Toggle theme and save 
-themeBtn.addEventListener("click", () => {
-  const isLightTheme = body.classList.contains("light-theme");
-  body.classList.toggle("light-theme", !isLightTheme);
-  body.classList.toggle("dark-theme", isLightTheme);
-  sunIcon.classList.toggle("mode", !isLightTheme);
-  moonIcon.classList.toggle("mode", isLightTheme);
-  localStorage.setItem("theme", isLightTheme ? "dark" : "light");
-});
